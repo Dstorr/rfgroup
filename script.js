ef05ed2c3ef86c9fe8b483fc5ac827e071954e16
Dstorr <82909041+Dstorr@users.noreply.github.com>
2022-04-24 18:35:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+    /*Coin price */
+var btc = document.getElementById("bitcoin");
+var eth = document.getElementById("ethereum");
+var ltc = document.getElementById("litecoin");
+var usdt = document.getElementById("tether");
+
+var settings = {
+    "async": true,
+    "scrossDomain": true,
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Clitecoin%2Ctether&vs_currencies=usd",
+    "method": "Get",
+    "headers": {}
+}
+
+$.ajax(settings).done(function (response) {
+    btc.innerHTML = response.bitcoin.usd;
+    eth.innerHTML = response.ethereum.usd;
+    ltc.innerHTML = response.litecoin.usd;
+    usdt.innerHTML = response.tether.usd;
+});
